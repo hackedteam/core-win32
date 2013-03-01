d65ea479ad7bd7023bbe1ade6cf20b2bf602bf66
naga <naga@hackingteam.it>
2013-03-01 11:04:03 +0100

    nuovo nome risorsa nel protocollo

--- ASP.cpp
+++ ASP.cpp
@@ -100,7 +100,7 @@ WCHAR *wRequest_array[] = {
 
 #define REQUEST_ARRAY_LEN 1
 WCHAR *wRequest_array[] = {
-		L"/index.php"
+		L"/default.aspx"
 	};
 
 HANDLE ASP_HostProcess = NULL; // Processo che gestisce ASP
